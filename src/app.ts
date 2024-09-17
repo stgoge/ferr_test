b7f2e13d7ec7d25ee755f71ba16c86dfc75c4252
German Baranov <german.baranov@geotoolkit.net>
2024-09-17 17:11:58 +0700

    [0.3.1] server.js logic moved to app.js

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,30 @@
+import fastify from "fastify";
+import view from "@fastify/view";
+import pug from "pug";
+
 import { Builder } from "./Builder.js";
 import { HTMLtoTextProcessor } from "./processors/HTMLtoTextProcessor.js";
 import { WordsToPdfListExporter } from "./exporters/WordsToPdfListExporter.js";
 import { FromSingleUrlLoader } from "./loaders/FromSingleUrlLoader.js";
 import { TextToWordsByLengthProcessor } from "./processors/TextToWordsByLengthProcessor.js";
-import { startServer } from "./server.js";
 
 const WORDS_COUNT = 10;
 const LONGEST = true;
+
+const PORT = 8080;
+
+const isUrl = (s: string) => {
+  try {
+    new URL(s);
+    return true;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err) {
+    return false;
+  }
+};
+
+const server = fastify();
+await server.register(view, { engine: { pug } });
 
 const builder = new Builder({
   loader: new FromSingleUrlLoader(),
@@ -19,4 +37,27 @@ const builder = new Builder({
   ),
 });
 
-startServer(builder);
+server.get("/", (req, res) => {
+  //@ts-expect-error 123
+  const { url } = req.query;
+
+  if (url) {
+    if (!isUrl(url)) {
+      return res.view("/src/views/index", { url, warning: true });
+    }
+
+    builder.build(url, res).then(() => {
+      res.code(200).send();
+    });
+  } else {
+    res.view("/src/views/index");
+  }
+});
+
+server.listen({ port: PORT }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});

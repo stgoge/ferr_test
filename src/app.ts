828f56b128c76a1964008ccfe37ae8163d9d8a72
German Baranov <german.baranov@geotoolkit.net>
2024-09-17 15:34:19 +0700

    Revert "[0.2.0] moved ListToPdf to exporters"
    This reverts commit f45ea33293edbc3baece91da1a61fa5925b4ca38.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { Builder } from "./Builder.js";
 import { HTMLtoTextProcessor } from "./processors/HTMLtoTextProcessor.js";
-import { ListToPdfExporter } from "./exporters/ListToPdfExporter.js";
+import { TextToPdfListProcessor } from "./processors/ListToPdfProcessor.js";
 import { LoadFromSingleUrlProcessor } from "./processors/LoadFromSingleUrlProcessor.js";
 import { WordsByLengthProcessor } from "./processors/WordsByLengthProcessor.js";
 import { initServer } from "./server.js";
@@ -13,10 +13,11 @@ const LONGEST = true;
 builder
   .addProcessor(new LoadFromSingleUrlProcessor())
   .addProcessor(new HTMLtoTextProcessor())
-  .addProcessor(new WordsByLengthProcessor(WORDS_COUNT, LONGEST));
+  .addProcessor(new WordsByLengthProcessor(WORDS_COUNT, LONGEST))
+  .addProcessor(
+    new TextToPdfListProcessor(
+      `Top ${WORDS_COUNT} ${LONGEST ? "longest" : "shortest"} words on page`
+    )
+  );
 
-const exporter = new ListToPdfExporter(
-  `Top ${WORDS_COUNT} ${LONGEST ? "longest" : "shortest"} words on page`
-);
-
-initServer(builder, exporter);
+initServer(builder);
